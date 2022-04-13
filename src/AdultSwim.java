a272ccd1512b7d39ba4bf777b7a2438285360f8c
Philip <philip1fox1@gmail.com>
2022-04-13 13:21:03 -0700

    first

---
+++ src/AdultSwim.java
@@ -0,0 +1,28 @@
+public class AdultSwim {
+
+    private static final int TEST = 1;
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        Thread.sleep(1500);
+        System.out.println("    8888888                   88888888");
+        Thread.sleep(400);
+        System.out.println("    8888                          8888");
+        Thread.sleep(400);
+        System.out.println("    8888     8888b.   .d8888b     8888");
+        Thread.sleep(400);
+        System.out.println("    8888        \"88b  88K         8888");
+        Thread.sleep(400);
+        System.out.println("    8888    .d888888  \"Y8888b.    8888");
+        Thread.sleep(400);
+        System.out.println("    8888    888  888       X88    8888 ");
+        Thread.sleep(400);
+        System.out.println("    8888    \"Y8888'8   88888P'    8888");
+        Thread.sleep(400);
+        System.out.println("    8888                          8888");
+        Thread.sleep(400);
+        System.out.println("    8888888                   88888888");
+        System.out.println();
+        Thread.sleep(10000);
+    }
+}
